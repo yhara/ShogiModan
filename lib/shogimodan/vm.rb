2116be07dcdc70c94015959414760e1375dd0162
Yutaka HARA <yutaka.hara+github@gmail.com>
2010-03-28 15:30:14 +0900

    Implemented VM

---
+++ lib/shogimodan/vm.rb
@@ -0,0 +1,108 @@
+# coding: utf-8
+
+class ShogiModan
+  class VM
+    def initialize(code, stdin=$stdin, stdout=$stdout)
+      @code, @stdin, @stdout = code, stdin, stdout
+      init
+    end
+
+    def init
+      @registers = [nil, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      @stack = []
+      @pc = 0
+      @labels = collect_labels(@code)
+    end
+    attr_reader :registers, :stack, :pc
+    private :init
+
+    def run
+      init
+
+      loop do 
+        op, arg1, arg2 = @code[@pc]
+        #p [@pc, op, arg1, arg2, @registers, @stack] if $DEBUG
+
+        case op
+        when nil
+          break
+        when :label
+          # do nothing
+        when :mov
+          set(arg1, get(arg2))
+        when :add
+          set(arg1, get(arg1) + get(arg2))
+        when :sub
+          set(arg1, get(arg1) - get(arg2))
+        when :mul
+          set(arg1, get(arg1) * get(arg2))
+        when :div
+          set(arg1, get(arg1).to_f / get(arg2))
+        when :mod
+          set(arg1, get(arg1) % get(arg2))
+        when :push
+          @stack.push get(arg1)
+        when :pop
+          val = @stack.pop || (raise "stack is empty")
+          set(arg1, val)
+        when :putc
+          if defined?(Encoding) then 
+            @stdout.print get(arg1).chr(Encoding::UTF_8)
+          else
+            @stdout.print get(arg1).chr
+          end
+        when :jump_if
+          if get(arg1) != 0
+            @pc = lookup_label(get(arg2)) - 1
+          end
+        when :jump_ifp
+          if get(arg1) >= 0
+            @pc = lookup_label(get(arg2)) - 1
+          end
+        else
+          raise "unknown opecode: #{op.inspect}"
+        end
+        @pc += 1
+      end
+    end
+
+    private 
+
+    def collect_labels(code)
+      {}.tap{|labels|
+        code.each_with_index{|(op, arg1, arg2), i|
+          if op == :label
+            check_register_idx(arg1)
+            labels[arg1] = i
+          end
+        }
+      }
+    end
+
+    def lookup_label(n)
+      if @labels.key?(n)
+        @labels[n]
+      else
+        raise ShogiModan::ProgramError, "label number #{n} is not found"
+      end
+    end
+
+    def get(i)
+      check_register_idx(i)
+      @registers[i]
+    end
+
+    def set(i, v)
+      check_register_idx(i)
+      @registers[i] = v
+    end
+
+    def check_register_idx(*args)
+      args.each do |i|
+        unless (0..9).include? i
+          raise "bad index of register: #{i}"
+        end
+      end
+    end
+  end
+end
---
+++ spec/spec_vm.rb
@@ -0,0 +1,120 @@
+# coding: utf-8
+require "#{File.expand_path File.dirname __FILE__}/spec_helper.rb"
+
+describe "ShogiModan::VM" do
+  context ".new" do
+    before :each do
+      @vm = ShogiModan::VM.new([])
+    end
+
+    it "should accept an array" do
+      @vm.should be_an_instance_of(ShogiModan::VM)
+    end
+
+    it "should initialize variables" do
+      @vm.registers.should == [nil, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      @vm.stack.should be_empty
+      @vm.pc.should == 0
+    end
+  end
+
+  context "#collect_labels" do
+    it "should collect all the labels" do
+      code = [[:label, 1], [:mov, 2, 3], [:label, 2]]
+      vm = ShogiModan::VM.new(code)
+      vm.send(:collect_labels, code).should == {1 => 0, 2 => 2}
+    end
+  end
+
+  context "#evaluate" do
+    def evaluate(code)
+      @vm = ShogiModan::VM.new(code, @in||$stdin, @out||$stdout)
+      @vm.run
+    end
+
+    it "should execute :mov" do
+      evaluate [[:mov, 1, 9]]
+      @vm.registers[1].should == 9
+    end
+
+    it "should execute :add" do
+      evaluate [[:add, 1, 9]]
+      @vm.registers[1].should == 10
+    end
+
+    it "should execute :sub" do
+      evaluate [[:sub, 1, 9]]
+      @vm.registers[1].should == -8
+    end
+
+    it "should execute :mul" do
+      evaluate [[:mul, 2, 9]]
+      @vm.registers[2].should == 18
+    end
+
+    it "should execute :div" do
+      evaluate [[:div, 8, 2]]
+      @vm.registers[8].should == 4
+      @vm.registers[8].should be_instance_of(Float)
+    end
+
+    it "should execute :mod" do
+      evaluate [[:mod, 8, 3]]
+      @vm.registers[8].should == 2
+      @vm.registers[8].should be_kind_of(Integer)
+    end
+
+    it "should execute :mod (when Float is expected)" do
+      evaluate [[:div, 1, 2], [:mod, 1, 3]]
+      @vm.registers[1].should == 0.5
+    end
+
+    it "should execute :push" do
+      evaluate [[:push, 1]]
+      @vm.stack.should == [1]
+    end
+
+    it "should execute :pop" do
+      evaluate [[:push, 1], [:pop, 2]]
+      @vm.registers[2].should == 1
+    end
+
+    it "should execute :putc" do
+      @out = mock($stdout)
+      @out.should_receive(:print).with("a")
+      evaluate [[:add, 5, 5], [:mul, 5, 5], [:sub, 5, 3],
+                [:putc, 5]]
+    end
+
+    if defined?(Encoding)
+      it "should execute :putc with kanji" do
+        @out = mock($stdout)
+        @out.should_receive(:print).with("✐")
+        evaluate [[:add, 5, 5], [:mul, 5, 5], [:mul, 5, 5],
+                  [:putc, 5]]
+      end
+    end
+
+    it "should execute :jump_if" do
+      evaluate [[:jump_if, 1, 1], [:mov, 2, 3], [:label, 1]]
+      @vm.registers[2].should == 2
+    end
+
+    it "should execute :jump_if, but should not jump if false(0)" do
+      evaluate [[:sub, 9, 9],
+                [:jump_if, 9, 1], [:mov, 2, 3], [:label, 1]]
+      @vm.registers[2].should == 3
+    end
+
+    it "should execute :jump_ifp" do
+      evaluate [[:jump_if, 1, 1], [:mov, 2, 3], [:label, 1]]
+      @vm.registers[2].should == 2
+    end
+
+    it "should execute :jump_if, but should not jump if false(0)" do
+      evaluate [[:sub, 8, 9],
+                [:jump_ifp, 8, 1], [:mov, 2, 3], [:label, 1]]
+      @vm.registers[2].should == 3
+    end
+  end
+end
